b52951556941b78b87069d2d53ac0bfbdf89df91
Daniel Barrientos Salas <danielbarrientossalas@hotmail.com>
2025-07-19 13:08:07 -0600

    resolucion a errores de sintaxis
    Resolucion de errores de sintaxis

--- login.php
+++ login.php
@@ -2,38 +2,39 @@
 $mysql = new mysqli("localhost", "user", "password", "usuarios_forlive");
 
 if ($mysql->connect_error) {
-	die("Error de conexion: " . $mysqli ->connect_error);
+    die("Error de conexion: " . $mysql->connect_error);
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$identificador = $_POST["identificador"];
-	$password = $_POST["password"];
+    $identificador = $_POST["identificador"];
+    $password = $_POST["password"];
 
-	$stmt = mysqli->prepare("SELECT nombre, apellidos, password FROM usuarios WHERE identificador = ?");
-	$stmt->bind_param("s", $identificador);
-	$stmt->execute();
-	$stmt->store_result();
+    $stmt = $mysql->prepare("SELECT nombre, apellidos, password FROM usuarios WHERE identificador = ?");
+    $stmt->bind_param("s", $identificador);
+    $stmt->execute();
+    $stmt->store_result();
 
-	if($stmt->num_rows > 0) {
-		$stmt->bind_result($nombre, $apellidos, $hash_guardado);
-		$stmt->fetch();
-	
-		if (password_verify($password, $hash_guardado)) {
-			echo "Bienvenido $nombre $apellidos;
-		} else {
-			echo "password incorrecto.";
-		}
-	} else {
-		echo "Usuario no encontrado";
-	}
+    if($stmt->num_rows > 0) {
+        $stmt->bind_result($nombre, $apellidos, $hash_guardado);
+        $stmt->fetch();
+    
+        if (password_verify($password, $hash_guardado)) {
+            echo "Bienvenido $nombre $apellidos";
+        } else {
+            echo "Password incorrecto.";
+        }
+    } else {
+        echo "Usuario no encontrado";
+    }
 
-	$stmt->close();
+    $stmt->close();
 }
+$mysql->close();
 ?>
 
-<h2>Iniciar Secion</h2>
+<h2>Iniciar Sesión</h2>
 <form method="POST">
-	Identificador: <input type="text" name="identificador" required><br>
-	Password> <input type="password" name="password" required><br>
-	<input type="submit" vlaue= "Ingresar">
+    Identificador: <input type="text" name="identificador" required><br>
+    Password: <input type="password" name="password" required><br>
+    <input type="submit" value="Ingresar">
 </form>
